e1e5c44d1878536bb85b989dd48e0d36c6c8fb50
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-09-19 15:39:27 -0400

    SD card logging

--- DrivelineRetrofit/DrivelineRetrofit.ino
+++ DrivelineRetrofit/DrivelineRetrofit.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <SD.h>
 #include <FlexCAN_T4.h>
 //----------------------------------------------------------------------------//
 
@@ -8,34 +9,55 @@
 #define INTERVAL      15 // ms
 #define ADC_PIN_COUNT 6
 
+#define CAN_BAUDRATE 500000 // 500 kbps
+
 #define ADC_RESOLUTION 10 // bits
 
 #define MSG_ID 0x18FF50E5 // Extended 29 bit ID
+
+#define SD_CARD_FILE_NAME "dline.csv"
 
 #define LED_PIN 13
 
 const uint8_t ADC_PINS[ADC_PIN_COUNT] = {14, 15, 16, 17, 18, 19};
 
-FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can1;
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can;
 
-unsigned long previousMillis = 0;
-bool ledState = false;
+unsigned long g_previousMillis = 0;
+bool g_ledState = false;
+bool g_sdCardInitialized = false;
 //----------------------------------------------------------------------------//
 
 
 //----------------------------------------------------------------------------//
 void setup()
 {
-	can1.begin();
-	can1.setBaudRate(500000); // Set CAN bitrate, 500kb
-
   	Serial.begin(115200);
 
+	Serial.println("Driveline Retrofit Board Starting...");
+
+	Serial.println("Initializing CAN...");
+	can.begin();
+	can.setBaudRate(CAN_BAUDRATE);
+
+
+	Serial.println("Configuring pins...");
 	pinMode(LED_PIN, OUTPUT);
 
 	for (uint8_t i = 0; i < ADC_PIN_COUNT; i++) {
 		pinMode(ADC_PINS[i], INPUT);
 	}
+
+	Serial.println("Initializing SD card...");
+	if (!SD.begin(BUILTIN_SDCARD)) {
+		Serial.println("Card failed, or not present.");
+		g_sdCardInitialized = false;
+	} else {
+		Serial.println("Card initialized.");
+		g_sdCardInitialized = true;
+	}
+
+
 
 }
 //----------------------------------------------------------------------------//
@@ -44,11 +66,11 @@ void setup()
 //----------------------------------------------------------------------------//
 void loop() {
 	// Interval timing -------------------------------------------------------//
-	unsigned long diff = millis() - previousMillis;
+	unsigned long diff = millis() - g_previousMillis;
 	if (diff < INTERVAL) {
   		return;
   	}
-	previousMillis = millis();
+	g_previousMillis = millis();
 	Serial.print("Interval: ");
 	Serial.println(diff);
 
@@ -77,10 +99,26 @@ void loop() {
 	msg.len = 8;
 	memcpy(msg.buf, msgData, 8);
 	msg.flags.extended = true;
-	can1.write(msg);
+	can.write(msg);
+
+	// Log to SD card if initialized -----------------------------------------//
+	if (g_sdCardInitialized) {
+		File logFile = SD.open(SD_CARD_FILE_NAME, FILE_WRITE);
+		if (logFile) {
+			logFile.print(millis());
+			for (uint8_t i = 0; i < ADC_PIN_COUNT; i++) {
+				logFile.print(",");
+				logFile.print(readings[i]);
+			}
+			logFile.println();
+			logFile.close();
+		} else {
+			Serial.println("Error opening log file.");
+		}
+	}
 
 	// Toggle LED state ------------------------------------------------------//
-  	ledState = !ledState;
+  	g_ledState = !g_ledState;
   	digitalWrite(LED_PIN, led);
 }
 //----------------------------------------------------------------------------//
